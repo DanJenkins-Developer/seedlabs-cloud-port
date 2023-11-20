728489a46e4f2d8160974ce7677984831938ab45
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-20 13:59:39 -0600

    names for test

--- google/create_from_image/variables.tf
+++ google/create_from_image/variables.tf
@@ -1,17 +1,17 @@
 variable "vpc_network_name" {
   description = "The name of the VPC network to create"
   type       = string
-    default    = "custom-vpc-network"
+    default    = "custom-vpc-network-test"
 }
 
 variable "subnet_name" {
   description = "The name of the subnet to create"
   type        = string
-  default     = "custom-subnet"
+  default     = "custom-subnet-test"
 }
 
 variable "instance_name" {
   description = "The name of the instance to create"
   type        = string
-  default     = "seed-labs-ubuntu-vm"
+  default     = "seed-labs-ubuntu-vm-test"
 }
