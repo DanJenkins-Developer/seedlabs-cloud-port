466699444707e469d8cd421bdf8a61188b2b7517
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-20 14:55:42 -0600

    python test

--- google/create_from_image/main.py
+++ google/create_from_image/main.py
@@ -2,4 +2,19 @@ import subprocess
 import shutil
 import os
 
-current_dir = os.getcwd()
+# current_dir = os.getcwd()
+
+
+user_base_dir = './user'
+init_dir = './init_vm'
+lab_name = 'lab'
+
+if not os.path.exists(user_base_dir):
+    os.makedirs(user_base_dir)
+
+for item in os.listdir(init_dir):
+    item_path = os.path.join(init_dir, item)
+    if os.path.isdir(item_path):
+        shutil.copytree(item_path, os.path.join(user_base_dir, item))
+    else:
+        shutil.copy(item_path, os.path.join(user_base_dir, item))
