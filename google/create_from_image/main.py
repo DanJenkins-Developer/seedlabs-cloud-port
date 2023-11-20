4cae165ac86f5f213394742798d4f35e27f5167b
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-20 15:29:24 -0600

    main

--- google/create_from_image/main.py
+++ google/create_from_image/main.py
@@ -7,14 +7,20 @@ import os
 
 user_base_dir = './user'
 init_dir = './init_vm'
-lab_name = 'lab'
+lab_name = '/lab'
 
+user_lab_dir = user_base_dir + lab_name
+print(user_lab_dir)
+
+# Create user directory
 if not os.path.exists(user_base_dir):
     os.makedirs(user_base_dir)
+
+os.makedirs(user_lab_dir)
 
 for item in os.listdir(init_dir):
     item_path = os.path.join(init_dir, item)
     if os.path.isdir(item_path):
-        shutil.copytree(item_path, os.path.join(user_base_dir, item))
+        shutil.copytree(item_path, os.path.join(user_lab_dir, item))
     else:
-        shutil.copy(item_path, os.path.join(user_base_dir, item))
+        shutil.copy(item_path, os.path.join(user_lab_dir, item))
